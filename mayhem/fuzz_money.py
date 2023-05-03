534f3519623ecc697f9b3d1b02d0a2bfacbb1dcf
Bailey Capuano <bcapuano@asu.edu>
2023-05-02 21:39:54 -0400

    Initial Mayhem Heroes commit

---
+++ mayhem/fuzz_money.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+import atheris
+import sys
+
+import fuzz_helpers
+
+with atheris.instrument_imports(include=['currency_converter']):
+    from currency_converter import CurrencyConverter, RateNotFoundError
+
+ctr = 0
+
+def TestOneInput(data):
+    global ctr
+
+    ctr += 1
+    fdp = fuzz_helpers.EnhancedFuzzedDataProvider(data)
+    f: str
+    with fdp.ConsumeTemporaryFile(suffix='.csv', as_bytes=False) as f:
+        try:
+            c = CurrencyConverter(f)
+            currency = fdp.PickValueInList(c.currencies)
+            new_currency = fdp.PickValueInList(c.currencies)
+            c.convert(fdp.ConsumeFloat(), currency, new_currency)
+        except (RateNotFoundError, StopIteration):
+            return -1
+        except Exception as e:
+            if ctr >= 50_000:
+                # We have iterated long enough for Mayhem to start up
+                raise e
+
+
+def main():
+    atheris.Setup(sys.argv, TestOneInput)
+    atheris.Fuzz()
+
+
+if __name__ == "__main__":
+    main()
